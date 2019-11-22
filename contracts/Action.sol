bb8742d10cba9ab3b3f2f4dd580d904af489aff5
pao741 <anuvit741@gmail.com>
2019-11-22 18:30:17 +0700

    fix bug in action.sol

--- contracts/Action.sol
+++ contracts/Action.sol
@@ -2,27 +2,24 @@ pragma solidity ^0.5.0;
 import "./DocumentRequest.sol";
 
 contract Action is DocumentRequest{
-    
     event Accept(uint256 id);
     event Reject(uint256 id);
-    
-    function accept(uint256 loanRequestID,uint256 index){
+    function accept(uint256 loanRequestID,uint256 id) external{
         uint256 n = LRmapping[loanRequestID].length;
-        for(uint256 i=0;i<n;i++){
-            if(i != index){
-                LRmapping[loanRequestID][index].isActive = false;
-                emit Accept(index);
+        for(uint256 i = 0; i < n; i++){
+            if(i != id){
+                LRmapping[loanRequestID][id].isActive = false;
+                emit Accept(id);
 
             }
         }
     }
-    
-    function reject(loanRequestID, index){
+    function reject(uint256 loanRequestID,uint256 id) external{
          uint256 n = LRmapping[loanRequestID].length;
-        for(uint256 i=0;i<n;i++){
-            if(i == index){
-                LRmapping[loanRequestID][index].isActive = false;
-                emit Reject(index);
+        for(uint256 i = 0; i < n; i++){
+            if(i == id){
+                LRmapping[loanRequestID][id].isActive = false;
+                emit Reject(id);
             }
         }
     }
